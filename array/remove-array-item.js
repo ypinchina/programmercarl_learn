66f693c19fee1d439676ec724bb73c10b4b5a049
yipeng <yip@augurit.com>
2023-12-13 09:23:01 +0800

    updata

--- array/remove-array-item.js
+++ array/remove-array-item.js
@@ -16,3 +16,21 @@ var removeElement = function(nums, val) {
 };
 
 // 空间复杂度O(1)，时间复杂度O(n)
+
+// 双指针的另一种写法
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  let i = 0, j = 0 
+  // 双指针
+  for (i; i < nums.length; i++) {
+      if (nums[i] !== val) {
+          nums[j] = nums[i]
+          j++
+      }
+  }
+  return j
+};
