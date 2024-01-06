7a2b731fe198cf10682f14aa86343f9ebd4d4c25
ypinchina <batmanyp@foxmail.com>
2024-01-06 21:50:13 +0800

    updata

---
+++ pointer/reverse-linked-list.js
@@ -0,0 +1,23 @@
+// 反转链表 https://leetcode.cn/problems/reverse-linked-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let pre = null;
+  let cur = head;
+  while (cur) {
+    const temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+  return pre;
+};
